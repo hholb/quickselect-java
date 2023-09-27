028d79cd42f6755fe235f11a94c06b82c47c63fd
hholb <hholbroo@unca.edu>
2023-09-27 14:44:58 -0400

    Add required classes and method signatures.

--- src/edu/unca/csci333/Main.java
+++ src/edu/unca/csci333/Main.java
@@ -1,7 +1,21 @@
+/* Hayden Holbrook
+ * CSCI-333 Fall 2023
+ * HW 04
+ */
 package edu.unca.csci333;
 
 public class Main {
+
+    private void countingSort(int[] input, int[] output, int largestValueInInput) {
+        // TODO: Implement counting sort
+    }
+
+    public int[] countingSort(int[] input) {
+        // TODO: Implement counting sort wrapper
+        return new int[0];
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        // TODO: Implement main
     }
 }
---
+++ src/edu/unca/csci333/Quickselect.java
@@ -0,0 +1,23 @@
+/* Hayden Holbrook
+ * CSCI-333 Fall 2023
+ * HW 04
+ */
+package edu.unca.csci333;
+
+public class Quickselect<T extends Comparable<T>> {
+    T[] arr;
+
+    public Quickselect(T[] arr) {
+        this.arr = arr;
+    }
+
+    public T randomizedQuickSelect(int orderStatistic) {
+        // TODO: Implement randomized quick select wrapper
+        return null;
+    }
+
+    private T randomizedQuickSelect(T[] arr, int lowerBound, int upperBound, int orderStatistic) {
+        // TODO: Implement randomized quick select
+       return null;
+    }
+}
