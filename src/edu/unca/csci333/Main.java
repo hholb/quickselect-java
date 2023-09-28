d2f1b15488f67349dd02f57dbf47f38440fdb869
hholb <hholbroo@unca.edu>
2023-09-28 15:31:18 -0400

    implement counting sort methods

--- src/edu/unca/csci333/Main.java
+++ src/edu/unca/csci333/Main.java
@@ -9,13 +9,34 @@ import java.util.Random;
 
 public class Main {
 
-  private void countingSort(int[] input, int[] output, int largestValueInInput) {
-    // TODO: Implement counting sort
+  private static void countingSort(int[] input, int[] output, int largestValueInInput) {
+    int[] counters = new int[largestValueInInput + 1];
+      for (int j : input) {
+          counters[j]++;
+      }
+    for (int i = 1; i <= largestValueInInput; i++) {
+      counters[i] = counters[i] + counters[i - 1];
+    }
+    for (int i = input.length - 1; i >= 0; i--) {
+      counters[input[i]]--;
+      output[counters[input[i]]] = input[i];
+    }
   }
 
-  public int[] countingSort(int[] input) {
-    // TODO: Implement counting sort wrapper
-    return new int[0];
+  public static int[] countingSort(int[] input) {
+    int largestValue = findLargestValue(input);
+    int[] output = new int[input.length];
+    countingSort(input, output, largestValue);
+    return output;
+  }
+
+  private static int findLargestValue(int[] input) {
+    int lagestSoFar = Integer.MIN_VALUE;
+      for (int j : input) {
+          if (j > lagestSoFar)
+              lagestSoFar = j;
+      }
+    return lagestSoFar;
   }
 
   /**
@@ -33,15 +54,40 @@ public class Main {
     }
   }
 
+  /**
+   * Randomly arranges the elements of the given array.
+   *
+   * @param array The array to mix up.
+   */
+  private static void scrambleArray(int[] array) {
+    Random rand = new Random();
+    for (int i = 0; i < array.length; i++) {
+      int randomIndexToSwap = rand.nextInt(array.length);
+      int temp = array[randomIndexToSwap];
+      array[randomIndexToSwap] = array[i];
+      array[i] = temp;
+    }
+  }
+
   public static void main(String[] args) {
     Integer[] arr = {1, 2, 3, 4, 5};
     scrambleArray(arr);
     int orderStat = 3;
+
+    int[] intArr = {1, 2, 3, 4, 5};
+    scrambleArray(intArr);
 
     System.out.println("Selecting order statistic " + orderStat + " from array:");
     System.out.println(Arrays.toString(arr));
     Quickselect<Integer> qs = new Quickselect<Integer>(arr);
     Integer result = qs.randomizedQuickSelect(orderStat);
     System.out.println("Result: " + result);
+
+    System.out.println();
+    System.out.println("Sorting...");
+    scrambleArray(arr);
+    System.out.println("Before: " + Arrays.toString(arr));
+    int[] sorted = countingSort(intArr);
+    System.out.println("After: " + Arrays.toString(sorted));
   }
 }
